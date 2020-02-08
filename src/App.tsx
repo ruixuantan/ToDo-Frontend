1ff98cc484a767bce2b13837d69a22236adfafcd
Rui Xuan <ruixuantan@yahoo.com.sg>
2020-02-08 16:01:02 +0800

    Initial configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import axios from "axios";
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./components/main/Home";
+import Dashboard from "./components/main/Dashboard";
+import NavBar from "./components/utils/NavBar";
+import Tasks from "./components/tasks/TaskIndex";
+import UpdateTask from "./components/tasks/UpdateTask";
+import TaskDetails from "./components/tasks/TaskDetails";
+import TagIndex from "./components/tags/TagIndex";
+import {User, UserData} from "./models/UserData";
+
+import "./App.scss";
+
+interface IState {
+  loggedInStatus: string,
+  user: User
 }
 
-export default App;
+export default class App extends Component {
+
+  state: IState = {
+    loggedInStatus: "NOT_LOGGED_IN",
+    user: {
+      id: 0,
+      email: ""
+    }
+  }
+
+  public checkLoginStatus = (): void => {
+    axios.get("https://todo-list-backend-rails.herokuapp.com/logged_in", {withCredentials: true})
+      .then(response => {
+        if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+            user: response.data.user
+          });
+        } else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            user: {}
+          });
+        }
+      })
+      .catch(error => {
+        console.log("error", error);
+      })
+  }
+
+  public componentDidMount(): void {
+    this.checkLoginStatus();
+  }
+
+  public handleLogin = (data: UserData): void => {
+    this.setState({
+      loggedInStatus: "LOGGED_IN",
+      user: data.user
+    });
+    window.location.href = "/dashboard";
+  }
+
+  public handleLogout = (): void => {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    });
+    window.location.href = "/";
+  }
+
+  public render() {
+    return (
+      <div>
+        <div className = "jumbotron" id = "app-header">
+          <h1>To-Do List</h1>
+        </div>
+        <BrowserRouter>
+        {this.state.loggedInStatus === "LOGGED_IN"
+            ? <NavBar handleLogout = {this.handleLogout}/>
+            : null}
+          <Switch>
+            <Route
+              path = "/"
+              exact
+              render = {props => (
+                <Home {...props}
+                  handleLogin = {this.handleLogin}
+                  handleLogout = {this.handleLogout}
+                  loggedInStatus = {this.state.loggedInStatus}
+                  />
+              )}
+              />
+            <Route
+              path = "/dashboard"
+              exact
+              component = {Dashboard}
+              />
+            <Route exact path = "/dashboard">
+              <Tasks />
+            </Route>
+            <Route
+              path = "/tasks/:id/edit"
+              exact
+              component = {UpdateTask}
+              />
+            <Route
+              path = "/tasks/:id"
+              exact
+              component = {TaskDetails}
+             />
+             <Route
+               path = "/tags"
+               exact
+               component = {TagIndex}
+              />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
